46699fd0038a41ad860c54a4411327c023026cda
calvin <calvinhaviandy@gmail.com>
2025-01-19 03:03:39 +0800

    new feature

---
+++ app/Models/Product.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'photo',
+        'about',
+        'tagline',
+        'price',
+        'price_per_person',
+        'duration',
+        'capacity',
+        'is_popular',
+    ];
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => [
+                'name' => $value,
+                'slug' => Str::slug($value),
+            ],
+        );
+    }
+
+    public function groups(): HasMany
+    {
+        return $this->hasMany(SubscriptionGroup::class);
+    }
+
+    public function keypoints(): HasMany
+    {
+        return $this->hasMany(ProductKeyPoint::class);
+    }
+}
---
+++ app/Models/SubscriptionGroup.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class SubscriptionGroup extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'max_capacity',
+        'participant_count',
+        'product_subscription_id',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function productSubscription(): BelongsTo
+    {
+        return $this->belongsTo( ProductSubscription::class, 'product_subscription_id');
+    }
+
+    public function groupMessages(): HasMany
+    {
+        return $this->hasMany( GroupMessage::class)->latest();
+    }
+
+    public function groupParticipants(): HasMany 
+    {
+        return $this->hasMany(GroupParticipant::class);
+    }
+
+}
